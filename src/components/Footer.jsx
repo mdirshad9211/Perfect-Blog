d386e0aee3cfee406415c2db60f35f1dcae0d4f8
MD IRSHAD <irshadidrisi9211@gmail.com>
2024-08-12 13:21:42 +0530

    completed footer from scratch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <ul className="footer_categories">
+        <li><Link to= "/posts/categories/information"> Information</Link></li>
+        <li><Link to= "/posts/categories/education"> Education</Link></li>
+        <li><Link to= "/posts/categories/investment"> Investment</Link></li>
+        <li><Link to= "/posts/categories/entertainment"> Entertainment</Link></li>
+        <li><Link to= "/posts/categories/anime"> Anime</Link></li>
+        <li><Link to= "/posts/categories/games"> Games</Link></li>
+        <li><Link to= "/posts/categories/job-openings"> Job Openings</Link></li>
+        <li><Link to= "/posts/categories/technology"> Technology</Link></li>
+        <li><Link to= "/posts/categories/uncategorized"> Uncategorized</Link></li>
+      </ul>
+
+      <div className="footer_copyright">
+        <small>All Rights Reserved &copy; Copyright, Perfect Notes.</small>
+      </div>
+    </footer>
   )
 }
 
