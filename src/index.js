6d0969a335df4c539ba8e89c8cde977ef6a2b440
MD IRSHAD <irshadidrisi9211@gmail.com>
2024-08-16 01:42:08 +0530

    created Dashboard

--- src/index.js
+++ src/index.js
@@ -7,19 +7,19 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
 import ErrorPage from"./pages/ErrorPage";
 
-import Home from "./pages/Home"
-import PostDetail from "./pages/PostDetail"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import UserProfile from "./pages/UserProfile"
-import Authors from "./pages/Authors"
-import CreatePost from "./pages/CreatePost"
-import CategoryPosts from "./pages/CategoryPosts"
-import Logout from "./pages/Logout"
-import AuthorPosts from "./pages/AuthorPosts"
-import Dashboard from "./pages/Dashboard"
-import EditPost from "./pages/EditPost"
-
+import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserProfile from "./pages/UserProfile";
+import Authors from "./pages/Authors";
+import CreatePost from "./pages/CreatePost";
+import CategoryPosts from "./pages/CategoryPosts";
+import Logout from "./pages/Logout";
+import AuthorPosts from "./pages/AuthorPosts";
+import Dashboard from "./pages/Dashboard";
+import EditPost from "./pages/EditPost";
+import DeletePost from "./pages/DeletePost"
 
 
 
@@ -42,6 +42,7 @@ const router = createBrowserRouter([
       {path: "myposts/:id", element: <Dashboard/>},
       {path: "logout", element: <Logout/>},
       {path: "posts/:id/edit", element: <EditPost/>},
+      {path: "posts/:id/delete", element: <DeletePost/>},
      ]
      
   }
